a6531412211c4037550becee4ed2900e1ac944ba
jfboeve <jfboeve@gmail.com>
2021-10-07 11:13:50 +0200

    change setting text

--- src/Key.js
+++ src/Key.js
@@ -84,7 +84,7 @@ export default class Key extends Lightning.Component {
         
         this.patch({
             Background: {color: hasFocus && focused ? focused : unfocused},
-            Label: {text: label, text: this._labelText, color: hasFocus && labelFocused ? labelFocused : labelUnfocused}
+            Label: {text: {text: label , ...this._labelText}, color: hasFocus && labelFocused ? labelFocused : labelUnfocused}
         });
     }
 
