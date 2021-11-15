17827eaf6167b140d8e98577261668d8e0618ffe
jfboeve <jfboeve@gmail.com>
2021-11-15 11:36:19 +0100

    fix collectionBase

--- src/helpers/CollectionBase.js
+++ src/helpers/CollectionBase.js
@@ -56,6 +56,7 @@ export default class CollectionBase extends Lightning.Component {
     }
 
     clear() {
+        console.log('clear')
         this._uids = [];
         this._items = [];
         if(this.wrapper) {
@@ -73,7 +74,8 @@ export default class CollectionBase extends Lightning.Component {
         this.addAt(item);
     }
 
-    addAt(item, index = this._items.length) {
+    addAt(item, index = this._items && this._items.length || 0) {
+        console.log('addAt', this._items)
         if(index >= 0 && index <= this._items.length) {
             if(!Array.isArray(item)) {
                 item = [item];
